9f99be766d6367c54ec366ea7a8ffd2ee0d52e8b
Hisham Zarka <hzarka@gmail.com>
2017-09-02 17:02:50 +0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jsql',
-    version='0.1',
+    version='0.2',
     author='Hisham Zarka',
     author_email='hzarka@gmail.com',
     packages = find_packages(),

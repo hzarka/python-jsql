30c37e612e5d8a3557095f26b586878208ff0039
Farouk Hjabo <fhjabo@noon.com>
2024-08-18 10:16:36 +0400

    upd version

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Lightweight wrapper around sqlalchemy + jinja2.
 
 ```
-pip install jsql==0.7
+pip install jsql==0.8
 ```
 
 ## Usage
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jsql',
-    version='0.7',
+    version='0.8',
     author='Hisham Zarka',
     author_email='hzarka@gmail.com',
     packages = find_packages(),
